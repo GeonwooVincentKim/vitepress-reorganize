9629456d49be61f54fa371588bc8739d841eccbf
GeonwooVincentKim <kdsnop@gmail.com>
2021-08-14 23:40:23 +0900

    [Update] - Successfully created Sidebar navigation part

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -93,10 +93,8 @@ export default {
             //         // ]
             //     }
             // ],
-            
 
-            // This Sidebar gets displayed when user is under `guide` directory
-            '/guide/': [
+            '/': [
                 {
                     text: 'Guide',
                     collapsible: true,
@@ -107,11 +105,7 @@ export default {
                         {text: 'One', link: '/guide/one'}, // /guide/one.md
                         {text: 'Two', link: '/guide/two'}, // /guide/two.md
                     ]
-                }
-            ],
-
-            // This Sidebar gets displayed when user is under `config` directory
-            '/config/': [
+                },
                 {
                     text: 'Config',
                     collapsible: true,
@@ -123,7 +117,38 @@ export default {
                         {text: 'Four', link: '/config/four'}, // /config/four.md
                     ]
                 }
-            ]
+            ],
+            
+
+            // This Sidebar gets displayed when user is under `guide` directory
+            // '/guide/': [
+            //     {
+            //         text: 'Guide',
+            //         collapsible: true,
+            //         collapsed: true,
+            //         items: [
+            //             // This shows `/guide/index.md` page.
+            //             {text: 'Index', link: '/guide/index'}, // /guide/index.md
+            //             {text: 'One', link: '/guide/one'}, // /guide/one.md
+            //             {text: 'Two', link: '/guide/two'}, // /guide/two.md
+            //         ]
+            //     }
+            // ],
+
+            // This Sidebar gets displayed when user is under `config` directory
+            // '/config/': [
+            //     {
+            //         text: 'Config',
+            //         collapsible: true,
+            //         collapsed: true,
+            //         items: [
+            //             // This shows `/config/index.md` page.
+            //             {text: 'Config', link: '/config/index'}, // /config/index.md
+            //             {text: 'Three', link: '/config/three'}, // /config/three.md
+            //             {text: 'Four', link: '/config/four'}, // /config/four.md
+            //         ]
+            //     }
+            // ]
         }
 
         // sidebar: [
