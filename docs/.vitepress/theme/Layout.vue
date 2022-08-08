5ae429ebce335dd8618d126ba4b48b998fd97a1e
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-08 10:35:06 +0900

    [Feature] - Added Layout.vue and added index.js file

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1>Custom Layout!</h1>
+
+    <!-- This is where markdown content will be rendered -->
+    <Content />
+</template>
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    },
+
+    components: {
+
+    },
+
+    computed: {
+
+    },
+
+    // asyncComputed: {
+
+    // },
+
+    watch: {
+
+    },
+
+    methods: {
+
+    }
+}
+</script>
