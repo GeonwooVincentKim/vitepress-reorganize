8d214771e31d07dfb3462f130c3ed149093021b3
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-08 11:56:27 +0900

    [Update] - Added nav functions

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,11 @@ export default {
 	title: "GeonwooKim's VitePress Project",
 	description: 'Learn Vitepress again',
     themeConfig: {
-        siteTile: "GeonwooKim's VitePress Project"
+        // siteTile: "GeonwooKim's VitePress Project"
+        nav: [
+            {text: 'Guide', link: '/guide'},
+            {text: 'Configs', link: '/configs'},
+            {text: 'Changelog', link: 'https://github.com/GeonwooVincentKim'}
+        ]
     }
 }
