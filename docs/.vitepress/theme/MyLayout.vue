59bb008dcf1ff0ca84872dba4d98a5b644c7c545
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-08 11:13:43 +0900

    [Feature] - Changed project design as renewal version

---
+++ docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <Layout>
+        <template #aside-outline-before>
+            My Custom Sidebar top content
+        </template>
+    </Layout>
+</template>
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,16 @@
 import Layout from './Layout.vue'
 // import Vue from 'vue'
 // import AsyncComputed from 'vue-async-computed'
+// import Theme from 'awesome-vitepress-theme'
+import DefaultTheme from 'vitepress/theme'
+import MyLayout from './MyLayout.vue'
 
 export default {
+    ...DefaultTheme,
+
     // root component to wrap each page
-    Layout,
+    Layout: MyLayout,
+    // Theme,
 
     // this is a Vue 3 functional component
     NotFound: () => 'custom 404',
@@ -13,5 +19,8 @@ export default {
         // app is the Vue 3 app instance from `createApp()`.
         // router is VitePress' custom router.
         // `siteData` is a `ref` of current site-level metadata.
+
+        // register global components
+        app.component('MyGlobalComponent', /* ... */)
     }
 }
